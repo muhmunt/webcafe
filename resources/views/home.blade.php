ac20b6a8cf3887fe1a9cbf62b0ef6f8da345dff0
rogersovich <dimasroger89@gmail.com>
2019-09-05 11:39:13 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,12 @@
     @if (Session::has('auth'))
         <div class="alert alert-danger">
             {{Session::get('auth')}}
-        </div>               
-    @endif                
+        </div>
+    @endif
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Welcome</div>
 
                 <div class="card-body">
                     @if (session('status'))
